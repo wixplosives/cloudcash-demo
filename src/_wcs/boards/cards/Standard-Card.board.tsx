baa19c44d02cc2e912e7ce1881d713ed56132391
mikawix <mikan@wix.com>
2022-08-18 19:13:02 +0300

    layout fixed

--- src/_wcs/boards/cards/Standard-Card.board.tsx
+++ src/_wcs/boards/cards/Standard-Card.board.tsx
@@ -3,23 +3,23 @@ import { createBoard } from "@wixc3/react-board";
 import { Cards } from "../../../components/cards/cards";
 
 export default createBoard({
-  name: "Standard Card",
-  Board: () => (
-    <Cards
-      incomePrice={1500}
-      outcomePrice={350}
-      balancePrice={28}
-      cardInfo={{
-        cardHolder: "Omer Kenet",
-        company: "Wix",
-        expiryDate: "06/25",
-      }}
-    />
-  ),
-  environmentProps: {
-    canvasHeight: 364,
-    canvasWidth: 597,
-    windowWidth: 938,
-    canvasBackgroundColor: "#ffffff",
-  },
+    name: "Standard Card",
+    Board: () => (
+        <Cards
+            incomePrice={1500}
+            outcomePrice={350}
+            balancePrice={28000}
+            cardInfo={{
+                cardHolder: "Omer Kenet",
+                company: "Wix",
+                expiryDate: "06/25",
+            }}
+        />
+    ),
+    environmentProps: {
+        canvasHeight: 364,
+        canvasWidth: 597,
+        windowWidth: 938,
+        canvasBackgroundColor: "#ffffff",
+    },
 });
