9673e5bbb9fe3990ac4add369a1fd450870c38c5
omerken <omerk@wix.com>
2022-08-28 19:12:44 +0300

    fix indentation

--- src/_wcs/boards/cards/Premium-Card.board.tsx
+++ src/_wcs/boards/cards/Premium-Card.board.tsx
@@ -3,24 +3,24 @@ import { createBoard } from "@wixc3/react-board";
 import { Cards } from "../../../components/cards/cards";
 
 export default createBoard({
-    name: "Premium Card",
-    Board: () => (
-        <Cards
-            incomePrice={1500}
-            outcomePrice={350}
-            balancePrice={20}
-            cardInfo={{
-                cardHolder: "Omer Kenet",
-                company: "Wix",
-                expiryDate: "06/23",
-                premium: true,
-            }}
-        />
-    ),
-    environmentProps: {
-        windowWidth: 829,
-        canvasBackgroundColor: "#ffffff",
-        windowHeight: 599,
-        canvasWidth: 612
-    },
+  name: "Premium Card",
+  Board: () => (
+    <Cards
+      incomePrice={1500}
+      outcomePrice={350}
+      balancePrice={20}
+      cardInfo={{
+        cardHolder: "Omer Kenet",
+        company: "Wix",
+        expiryDate: "06/23",
+        premium: true,
+      }}
+    />
+  ),
+  environmentProps: {
+    windowWidth: 829,
+    canvasBackgroundColor: "#ffffff",
+    windowHeight: 599,
+    canvasWidth: 612,
+  },
 });
