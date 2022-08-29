6dc7399ace05d93a28e67519b7e2486e2143eca9
omerken <omerk@wix.com>
2022-08-29 10:33:36 +0300

    Fixed styles of Cards component

--- src/_wcs/boards/cards/Standard-Card.board.tsx
+++ src/_wcs/boards/cards/Standard-Card.board.tsx
@@ -3,21 +3,22 @@ import { createBoard } from "@wixc3/react-board";
 import { Cards } from "../../../components/cards/cards";
 
 export default createBoard({
-    name: "Standard Card",
-    Board: () => (
-        <Cards
-            incomePrice={1500}
-            expensesPrice={350}
-            balancePrice={28}
-            cardInfo={{
-                cardHolder: "Omer Kenet",
-                company: "Wix",
-                expiryDate: "06/25",
-            }}
-        />
-    ),
-    environmentProps: {
-        canvasHeight: 364, windowWidth: 938,
-        canvasBackgroundColor: "#ffffff",
-    },
+  name: "Standard Card",
+  Board: () => (
+    <Cards
+      incomePrice={1500}
+      expensesPrice={350}
+      balancePrice={28}
+      cardInfo={{
+        cardHolder: "bsdfdsf adfhasf",
+        company: "Google",
+        expiryDate: "06/25",
+      }}
+    />
+  ),
+  environmentProps: {
+    canvasHeight: 364,
+    windowWidth: 940,
+    canvasBackgroundColor: "#ffffff",
+  },
 });
